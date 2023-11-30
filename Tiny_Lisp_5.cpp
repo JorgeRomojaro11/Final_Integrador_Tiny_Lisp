89751129772a73c882aeeb74754015f96e32f28d
JorgeRomojaro11 <jromodel@myuax.com>
2023-11-30 12:14:12 +0100

    .

--- Tiny_Lisp_5.cpp
+++ Tiny_Lisp_5.cpp
@@ -11,3 +11,22 @@ struct ColorConsole
     static constexpr auto bg_white = "\033[47m";
     static constexpr auto reset = "\033[0m";
 };
+struct ConsoleBox
+{
+    void new_text() { /*...*/ }
+    void set_text(const string &text) { cout << text << endl; }
+};
+
+ConsoleBox *consoleBox = new ConsoleBox;
+void load_script(const char* filename, bool show_script = false)
+{
+    try
+    {
+        ifstream file(filename, ios::binary);
+
+        if (!file.is_open())
+        {
+            cerr << "Error de apertura de " << filename << endl;
+            return;
+        }
+
