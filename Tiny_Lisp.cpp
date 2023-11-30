28bdd04da3671527572f5cc427fe65b6e2062294
JorgeRomojaro11 <jromodel@myuax.com>
2023-11-30 11:45:15 +0100

    .

--- Tiny_Lisp.cpp
+++ Tiny_Lisp.cpp
@@ -4,3 +4,10 @@
 
 void load_script(const char* filename, bool show_script = false) {
     std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Error: No se pudo abrir el archivo '" << filename << "'.\n";
+        return;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
